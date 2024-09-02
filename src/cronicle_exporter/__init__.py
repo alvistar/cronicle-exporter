36527d4c59f6fa8bbf9611b56b9d382ea29bc8d5
avigano <alvistar@gmail.com>
2024-09-02 17:04:56 +0200

    Added gauges

--- src/cronicle_exporter/__init__.py
+++ src/cronicle_exporter/__init__.py
@@ -1,6 +1,8 @@
+import time
+
 import click
 import logging
-
+from prometheus_client import start_http_server, Gauge
 from cronicle_exporter.api_client import ApiClient
 
 # Configuring logging
@@ -8,6 +10,11 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
+
+# Define Prometheus metrics
+TIME_START = Gauge("cronicle_event_time_start", "Event start time", ["event_title"])
+ELAPSED = Gauge("cronicle_event_elapsed", "Event elapsed time", ["event_title"])
+EXIT_CODE = Gauge("cronicle_event_exit_code", "Event exit code", ["event_title"])
 
 
 @click.command()
@@ -18,12 +25,26 @@ def main(port: int, url: str, api_key: str) -> int:
     logging.info(f"Initializing API client with base URL: {url}")
     api_client = ApiClient(url, api_key)
 
-    logging.info("Fetching schedule")
-    schedule = api_client.get_schedule()
-    logging.info(f"Retrieved {len(schedule)} schedule events")
+    # Start up the server to expose the metrics.
+    start_http_server(port)
     logging.info(f"Cronicle-exporter listening on port {port}")
-    # TODO: Implement exporter logic here
-    return 0
+
+    while True:
+        logging.info("Fetching schedule")
+        schedule = api_client.get_schedule()
+        logging.info(f"Retrieved {len(schedule)} schedule events")
+
+        for event in schedule:
+            last_event = api_client.get_last_event(event["id"])
+            event_title = last_event["event_title"]
+
+            if last_event:
+                TIME_START.labels(event_title).set(last_event["time_start"])
+                ELAPSED.labels(event_title).set(last_event["elapsed"])
+                EXIT_CODE.labels(event_title).set(last_event["code"])
+
+        # Sleep for a while before the next update
+        time.sleep(60)  # Update every minute
 
 
 if __name__ == "__main__":
